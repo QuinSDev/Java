193d5eb608e6fc61d97af30bdd4a87fb7dfa55fb
QuinSDev <quindev97@gmail.com>
2023-05-30 10:59:18 -0500

    Se crea la clase Cliete y ListCliente, que contendra una lista de clientes que se irán almacenando

---
+++ Herencia/BrisaNautica/src/alquilerbarco/cliente/Cliente.java
@@ -0,0 +1,32 @@
+package alquilerbarco.cliente;
+
+public class Cliente {
+    
+    private String nombre;
+    private long documento;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, long documento) {
+        this.nombre = nombre;
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(long documento) {
+        this.documento = documento;
+    }
+    
+}
---
+++ Herencia/BrisaNautica/src/alquilerbarco/cliente/ListCliente.java
@@ -0,0 +1,36 @@
+package alquilerbarco.cliente;
+
+import java.util.ArrayList;
+
+public class ListCliente {
+    
+    private ArrayList<Cliente> listCliente;
+
+    public ListCliente() {
+        listCliente = new ArrayList<>();
+    }
+
+    public ListCliente(ArrayList<Cliente> listCliente) {
+        this.listCliente = listCliente;
+    }
+
+    public ArrayList<Cliente> getListCliente() {
+        return listCliente;
+    }
+
+    public void setListCliente(ArrayList<Cliente> listCliente) {
+        this.listCliente = listCliente;
+    }
+    
+    public Cliente buscarCliente(long document) {
+        
+        for (Cliente cliente : listCliente) {
+            if (cliente.getDocumento() == document) {
+                return cliente;
+            }
+        }
+        return null;
+        
+    }
+    
+}
